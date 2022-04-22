52a48b8e06eb3ed36976ce778535088488fb100c
Josip Markovic <bozadesign@gmail.com>
2022-04-22 11:25:12 +0200

    Change colors. Add submit to form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,21 +1,48 @@
-import { FC } from "react";
+import { FC, FormEvent, useState } from "react";
 
 import "./Login.css";
 
 const Login: FC = () => {
+  const [emailValue, setEmailValue] = useState<string>("");
+  const [passwordValue, setPasswordValue] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(emailValue, passwordValue);
+    setEmailValue("");
+    setPasswordValue("");
+  };
+
   return (
-    <div className="login">
-      <h3>Please enter your credentials</h3>
+    <form className="login" onSubmit={handleSubmit}>
+      <h3>Login</h3>
       <div>
         <label>Email</label>
-        <input type="text" placeholder="enter email" />
+        <input
+          type="text"
+          value={emailValue}
+          placeholder="enter email"
+          onChange={(e) => setEmailValue(e.target.value)}
+          required
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+          title="Please enter valid email"
+        />
       </div>
       <div>
         <label>Password</label>
-        <input type="text" placeholder="enter password" />
+        <input
+          type="password"
+          value={passwordValue}
+          placeholder="enter password"
+          onChange={(e) => setPasswordValue(e.target.value)}
+          required
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+          title="Must contain at least one  number and one uppercase and 
+          lowercase letter, and at least 8 or more characters"
+        />
       </div>
       <button type="submit">Login</button>
-    </div>
+    </form>
   );
 };
 
